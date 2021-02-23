b7688c29daf9d163517e3d435ac439ae0d58617f
florian.schneider <florian.schneider@codecentric.de>
2021-02-23 11:31:18 +0100

    Inital project setup

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    TODO("Start implementing your code here")
+}
+
+fun scoring(team: String, score: Int) {
+    TODO("Not yet implemented")
+}
+
+fun showScoreBoard() {
+    TODO("Not yet implemented")
+}
